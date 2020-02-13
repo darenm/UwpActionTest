bcbd6b892aadc7e23ab98854d6dce3aefdadf8ec
Daren May <daren.may@gmail.com>
2020-02-12 19:08:14 -0700

    Add project files.

---
+++ UwpActionTest.Lib.Tests/PersonTests.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UwpActionTest.Lib.Models;
+
+namespace UwpActionTest.Lib.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void Person_Initialize()
+        {
+            var p = new Person();
+
+            Assert.IsNotNull(p);
+        }
+
+        public void Person_Firstname()
+        {
+            var p = new Person();
+            p.Age = 23;
+
+            Assert.AreEqual(23, p.Age);
+        }
+    }
+}
---
+++ UwpActionTest.Lib/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpActionTest.Lib.Models
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+    }
+}
